66bba6a043b46f8c76cb4fd683f4c82e235038ca
Lilian_may <artur2005kuzmin@gmail.com>
2025-05-06 17:42:37 +0400

    rename/new_name_for_excelhelper

---
+++ IAT_Test/DatabaseHelper.cs
@@ -0,0 +1,30 @@
+﻿using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+namespace IAT_Test
+{
+    using DotNetEnv;
+    using OfficeOpenXml;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    public class DatabaseHelper
+    {
+        public static string GetConnectionString()
+        {
+            Env.Load();
+
+            string server = Environment.GetEnvironmentVariable("DB_SERVER");
+            string database = Environment.GetEnvironmentVariable("DB_NAME");
+            string user = Environment.GetEnvironmentVariable("DB_USER");
+            string password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            return $"Server={server};Database={database};User Id={user};Password={password};";
+        }
+    }
+
+}
